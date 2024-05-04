e2ce3a6abaca6a0947601845c36d963b64611071
Usman Rasheed <93085761+UsmanRasheed1@users.noreply.github.com>
2024-05-04 11:31:01 +0500

    Update footer.blade.php
    Commit #7

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -8,9 +8,9 @@
                 <span class="footer-text">Black <span class="red-text">Tag</span> Boutique</span>
             </div>
             <div class="col-md-4 text-right"> <!-- Rest on the right -->
-                <span class="footer-made-by">Made by: Usman, Muneeb, and Talha</span>
+                <span class="footer-made-by">Made by: Usman Rasheed</span>
                 <a href="https://mail.google.com/mail/?view=cm&fs=1&to=k213225@nu.edu.pk" class="footer-email-link">Email us for any Queries!</a>
-               
+                <a href="https://github.com/UsmanRasheed1" class="footer-email-link">Follow me on GitHub!</a>
             </div>
         </div>
     </div>
